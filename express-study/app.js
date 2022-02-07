64ce62a669adcce3508ed668641fa3e02525bad8
cloer <chltjrrb97@gmail.com>
2022-02-06 23:00:20 -0500

    ADD: html serving

--- express-study/app.js
+++ express-study/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.set("port", process.env.PORT || 8080);
 
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/hello", (req, res) => {
   res.send("hello express");
 });
 
