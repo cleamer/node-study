22cf1b850454798743276a420640abf4200e03cb
cloer <chltjrrb97@gmail.com>
2022-03-08 22:57:17 -0500

    ADD: web socket template

---
+++ websocket/ws-module/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ws = require('ws');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+app.set('port', process.env.PORT || 8003);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.use((req, res, next) => {
+  const notFoundError = new Error(`${req.method} ${req.url} NotFound`);
+  notFoundError.status = 404;
+  next(notFoundError);
+});
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== ' production' ? err : {};
+  res.status(err.status || 500).render('error');
+});
+
+const expressServer = app.listen(app.get('port'), () => console.log('ws module server is running on port ' + app.get('port')));
